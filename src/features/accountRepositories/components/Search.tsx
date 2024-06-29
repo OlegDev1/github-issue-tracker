772cf3008d53a8d453583fc2a026023b28643320
Fykapu <jsjwhshjsig@gmail.com>
2024-06-29 14:41:09 +0200

    Ability to cancel the search of repositories by name

--- src/features/accountRepositories/components/Search.tsx
+++ src/features/accountRepositories/components/Search.tsx
@@ -2,23 +2,52 @@ import { useState } from "react";
 import { Button } from "../../../components/ui/button";
 import { Input } from "../../../components/ui/input";
 import "./Search.css";
+import { SetURLSearchParams } from "react-router-dom";
 
-export default function Search({ setSearchParams, searchQuery, searchParams }) {
+type SearchProps = {
+  setSearchParams: SetURLSearchParams;
+  searchQuery: string | null;
+  searchParams: URLSearchParams;
+};
+
+export default function Search({ setSearchParams, searchQuery, searchParams }: SearchProps) {
   const [inputText, setInputText] = useState(searchQuery);
   const searchParamsObj = Object.fromEntries(searchParams.entries());
 
   return (
     <div className="searchBar">
-      <Input
-        className="searchBar__input"
-        placeholder="Search by name..."
-        value={inputText}
-        onChange={(e) => setInputText(e.target.value)}
-      />
+      <div className="searchBar__input">
+        <Input
+          className="searchBar__input-input"
+          placeholder="Search by name..."
+          value={inputText === null ? "" : inputText}
+          onChange={(e) => setInputText(e.target.value)}
+        />
+        {searchQuery !== null && (
+          <svg
+            onClick={() => {
+              delete searchParamsObj.query;
+              setSearchParams({ ...searchParamsObj, page: "1" });
+              setInputText(null);
+            }}
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6 searchBar__cancel-button">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+          </svg>
+        )}
+      </div>
       <Button
-        onClick={() => {
-          setSearchParams({ ...searchParamsObj, page: "1", query: inputText });
-        }}>
+        onClick={() =>
+          setSearchParams({
+            ...searchParamsObj,
+            page: "1",
+            query: inputText === null ? "" : inputText,
+          })
+        }>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
